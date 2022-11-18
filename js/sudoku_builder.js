eb1afd30132754d227e1d7f0eb5157cc8f73fb36
sigh <sigh.projects@gmail.com>
2022-11-18 22:07:53 +1100

    Optimize renban using a dedicated handler.
    Reduces the number of guesses by 8x (5861 -> 742) and the search time by
    10x (~50ms -> ~5ms).

--- js/sudoku_builder.js
+++ js/sudoku_builder.js
@@ -435,7 +435,7 @@ class SudokuConstraint {
     constructor(difference, ...cells) {
       // German whisper lines omit the difference, so the
       // first argument is actually a cell
-      if (difference!=+difference) {
+      if (difference != +difference) {
         cells.unshift(difference);
         difference = 5;
       }
@@ -921,14 +921,7 @@ class SudokuBuilder {
         case 'Renban':
           cells = constraint.cells.map(c => shape.parseCellId(c).cell);
           yield new SudokuConstraintHandler.AllDifferent(cells);
-          let diff = cells.length;
-          for (let i = 0; i < cells.length; i++) {
-            for (let j = i+1; j < cells.length; j++) {
-              yield new SudokuConstraintHandler.BinaryConstraint(
-                cells[i], cells[j], (a, b) => Math.abs(a-b)<diff
-                );
-            }
-          }
+          yield new SudokuConstraintHandler.Renban(cells);
           break;
 
         case 'RegionSumLine':
