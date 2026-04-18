a5afd20bb76ea64ffdd168db8257d3a535192862
sigh <sigh.projects@gmail.com>
2026-04-18 14:56:08 +1000

    Add Rectangle Sums example.

--- data/collections.js
+++ data/collections.js
@@ -497,6 +497,13 @@ export const EXAMPLES = [
     constraintTypes: ['NFA'],
   },
   {
+    name: 'Rectangle Sums',
+    src: 'https://www.youtube.com/watch?v=T03vUTiJBjk',
+    input: '/data/rectangle_sums.iss',
+    solution: 'BFGDAHEICCEAFBIHDGDHIEGCABFACHIEFDGBFGEHDBICAIBDGCAFEHGIFCHEBADHDBAIGCFEEACBFDGHI',
+    constraintTypes: ['Or', 'And', 'Var', 'Sum'],
+  },
+  {
     name: 'Embedded Squishdoku',
     src: 'https://logic-masters.de/Raetselportal/Raetsel/zeigen.php?id=000QRD',
     input: '.NoBoxes.AllDifferent~R1C1~R1C2~R1C3~R2C3~R3C3~R3C2~R3C1~R2C1~R2C2.AllDifferent~R1C3~R2C3~R3C3~R3C4~R3C5~R2C5~R1C5~R1C4~R2C4.AllDifferent~R1C5~R2C5~R3C5~R3C6~R3C7~R2C7~R1C7~R1C6~R2C6.AllDifferent~R3C1~R3C2~R3C3~R4C3~R5C3~R5C2~R5C1~R4C1~R4C2.AllDifferent~R3C3~R4C3~R5C3~R5C4~R5C5~R4C5~R3C5~R3C4~R4C4.AllDifferent~R3C5~R4C5~R5C5~R5C6~R5C7~R4C7~R3C7~R3C6~R4C6.AllDifferent~R5C7~R5C6~R5C5~R6C5~R7C5~R7C6~R7C7~R6C7~R6C6.AllDifferent~R7C5~R6C5~R5C5~R5C4~R5C3~R6C3~R7C3~R7C4~R6C4.AllDifferent~R5C1~R5C2~R5C3~R6C3~R7C3~R7C2~R7C1~R6C1~R6C2.Arrow~R9C2~R8C3~R7C4~R6C5~R5C6~R4C7~R3C8.Arrow~R9C9~R8C9~R8C8.SumLine~10~R9C1~R8C1~R7C1~R6C1~R6C2~R7C2.SumLine~10~R2C2~R2C3~R2C4~R2C5.SumLine~10~R8C4~R9C4~R9C5~R9C6~R9C7~R9C8',
